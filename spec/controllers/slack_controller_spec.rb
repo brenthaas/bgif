b23c87ef1ad737a5c0a5a9252f2ab9fbb639b06c
Brent Haas <brenthaas@gmail.com>
2016-06-15 23:50:18 -0700

    Respond with ephemeral text response when can't find a gif

--- spec/controllers/slack_controller_spec.rb
+++ spec/controllers/slack_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe SlackController do
   describe '#create' do
+    let(:json_response) { JSON.parse(response.body) }
+    let(:command_tags) { %w(foo bar) }
     let(:slack_command_params) do
       {
         token: 'gIkuvaNzQIHg97ATvDxqgjtO',
@@ -12,7 +14,7 @@ RSpec.describe SlackController do
         user_id: 'U2147483697',
         user_name: 'Steve',
         command: '/weather',
-        text: '94070',
+        text: command_tags.join(' '),
         response_url: 'https://hooks.slack.com/commands/1234/5678'
       }
     end
@@ -20,5 +22,16 @@ RSpec.describe SlackController do
     subject(:post_create) { post :create, params: slack_command_params }
 
     it { is_expected.to have_http_status :ok }
+
+    context "when there is no gif with the given tags" do
+      before do
+        allow(Gif).to receive(:tagged_with).with(command_tags).and_return([])
+      end
+
+      it 'gives an ephemeral response about the failure' do
+        post_create
+        expect(json_response['response_type']).to eq 'ephemeral'
+      end
+    end
   end
 end
